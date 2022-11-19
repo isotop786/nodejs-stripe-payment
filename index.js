bfc74ea20762dc63c4976c7351f4f3ce6ede91b1
isotop786 <marufislam77@gmail.com>
2022-11-19 14:00:09 +0600

    cors enabled

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const stripe = require('stripe')('sk_test_51L04iFCqX00FzFwQTK7PrsxLqw1FObBUQlR6elY3arnIiimeMhfwihpLFDmh19cyllZfbLsJ2s2mHL5YNNywmHgz00vsHopuO6')
+const cors = require('cors');
 
 const app = express()
 const PORT = process.env.PORT || 5999
 
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/create-payment-intent', async (req, res) => {
