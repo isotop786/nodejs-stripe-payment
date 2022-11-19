9eae9bcd3f2be8ed3d945a5260c43527176b4fbb
isotop786 <marufislam77@gmail.com>
2022-11-19 13:50:02 +0600

    first_commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const stripe = require('stripe')('sk_test_51L04iFCqX00FzFwQTK7PrsxLqw1FObBUQlR6elY3arnIiimeMhfwihpLFDmh19cyllZfbLsJ2s2mHL5YNNywmHgz00vsHopuO6')
+
+const app = express()
+const PORT = process.env.PORT || 5999
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/create-payment-intent', async (req, res) => {
+    var amount = req.body.amount
+    console.log(amount);
+    try {
+        
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 200.00,
+            currency:'usd'
+        })
+        res.json({clientSecret: paymentIntent.client_secret})
+    }
+    catch (err) {
+        console.log(err);
+    }
+    
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running in port ${PORT}...`);
+})
